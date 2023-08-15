06a49c3780dfe85f0433cfd53d3e591b2fe7745d
Hjalte Hesbjerg <hjalte@Hjaltes-MacBook-Pro.local>
2023-08-15 23:39:24 +0900

    Added Null object

--- object/object.go
+++ object/object.go
@@ -8,6 +8,7 @@ type ObjectType string
 const (
 	INTEGER_OBJ = "INTEGER"
 	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ = "NULL"
 )
 
 type Object interface {
@@ -27,4 +28,9 @@ type Boolean struct {
 }
 
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
-func (b *Boolean) Inspect() string { return fmt.Sprintf("%d", b.Value) }
+func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
+
+type Null struct {}
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string { return "null" }
